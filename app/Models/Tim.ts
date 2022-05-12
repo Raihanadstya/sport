62051e3376d461d4da826e1f3b0f70519d718fc1
farrellh1 <farrell.hauzan@gmail.com>
2022-05-12 13:32:21 +0700

    refactoring tim code

--- app/Models/Tim.ts
+++ app/Models/Tim.ts
@@ -1,36 +1,64 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
-import Pemain from './Pemain'
+import { DateTime } from "luxon";
+import { BaseModel, column, HasMany, hasMany } from "@ioc:Adonis/Lucid/Orm";
+import Pemain from "./Pemain";
 
 export default class Tim extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column()
-  public nama: string
+  public nama: string;
 
   @column()
-  public asalInstansi: string
-  
+  public asalInstansi: string;
+
   @column()
-  public deskripsi?: string
-  
+  public deskripsi?: string;
+
   @column()
-  public logo?: string
-  
+  public logo?: string;
+
   @column()
-  public userId: number
+  public userId: number;
+
+  @column()
+  public provinsi: string;
+
+  @column()
+  public wilayah: string;
+
+  @column()
+  public website: string;
+
+  @column()
+  public facebook: string;
+
+  @column()
+  public twitter: string;
+
+  @column()
+  public instagram: string;
+
+  @column()
+  public youtube: string;
+
+  @column()
+  public tiktok: string;
 
   @hasMany(() => Pemain, {
-    foreignKey: "id"
+    foreignKey: "id",
   })
-  public pemains: HasMany<typeof Pemain>
+  public pemains: HasMany<typeof Pemain>;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
-    validated: Promise<{ nama: string; asalInstansi: string; deskripsi: string | undefined }>
-    tim: { nama: string; asalInstansi: string; deskripsi: string | undefined }
+  public updatedAt: DateTime;
+  validated: Promise<{
+    nama: string;
+    asalInstansi: string;
+    deskripsi: string | undefined;
+  }>;
+  tim: { nama: string; asalInstansi: string; deskripsi: string | undefined };
 }
