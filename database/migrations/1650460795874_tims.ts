e593f541c41085df739f004649cfd1212813ad97
ShiroXRai <raihanvans@gmail.com>
2022-05-12 07:41:01 +0700

    update migration

--- database/migrations/1650460795874_tims.ts
+++ database/migrations/1650460795874_tims.ts
@@ -1,21 +1,30 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class Tims extends BaseSchema {
-  protected tableName = 'tims'
+  protected tableName = "tims";
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
-      table.string('nama', 150).notNullable()
-      table.string('asal_instansi', 100).notNullable()
-      table.text('deskripsi').nullable()
-      table.text('logo').nullable()
-      table.integer('user_id').unsigned().notNullable()
-      table.timestamps(true, true)
-    })
+      table.increments("id").primary();
+      table.string("nama", 150).notNullable();
+      table.string("asal_instansi", 100).notNullable();
+      table.text("deskripsi").nullable();
+      table.text("logo").nullable();
+      table.integer("user_id").unsigned().notNullable();
+      table.string("provinsi");
+      table.string("wilayah");
+      table.string("website");
+      table.string("facebook");
+      table.string("twitter");
+      table.string("instagram");
+      table.string("youtube");
+      table.string("tiktok");
+      table.boolean("dihapus").defaultTo(0);
+      table.timestamps(true, true);
+    });
   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
+  public async down() {
+    this.schema.dropTable(this.tableName);
   }
 }
--- database/migrations/1650460808072_pemains.ts
+++ database/migrations/1650460808072_pemains.ts
@@ -1,21 +1,34 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class Pemains extends BaseSchema {
-  protected tableName = 'pemains'
+  protected tableName = "pemains";
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
-      table.string('posisi', 50).notNullable()
-      table.string('nomor_punggung', 3).notNullable()
-      table.text('foto').nullable()
-      table.integer('user_id').unsigned().notNullable()
-      table.integer('tim_id').unsigned().references('id').inTable('tims').onDelete('CASCADE')
-      table.timestamps(true, true)
-    })
+      table.increments("id").primary();
+      table.string("posisi", 50).notNullable();
+      table.string("nomor_punggung", 3).notNullable();
+      table.string("nickname").notNullable();
+      table.string("deskripsi").notNullable();
+      table.text("foto").nullable();
+      table.integer("user_id").unsigned().notNullable();
+      table
+        .integer("tim_id")
+        .unsigned()
+        .references("id")
+        .inTable("tims")
+        .onDelete("CASCADE");
+      table.string("facebook");
+      table.string("twitter");
+      table.string("instagram");
+      table.string("youtube");
+      table.string("tiktok");
+      table.boolean("dihapus").defaultTo(0);
+      table.timestamps(true, true);
+    });
   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
+  public async down() {
+    this.schema.dropTable(this.tableName);
   }
 }
