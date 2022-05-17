609a728a53b1ab0df19a1d896228f6e011125412
Rochim <rochim.rci26@gmail.com>
2022-05-17 10:28:45 +0700

    add .env to build folder

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,6 @@ module.exports = {
       exec_mode: "cluster",
       instances: "max",
       env_production: {
-        NODE_ENV: "production",
         PORT: 3333,
         HOST: "0.0.0.0",
         NODE_ENV: "development",
@@ -31,7 +30,24 @@ module.exports = {
       repo: "git@github.com:go-sport-indonesia/tim-service.git",
       ref: "origin/main",
       key: "~/.ssh/id_rsa",
-      "post-deploy": "npm i; node ace build; pm2 reload ecosystem.config.js",
+      "post-deploy": `
+      npm i; 
+      node ace build;  
+      echo 'PORT: 3333' >> ./build/.env;
+      echo 'HOST: 0.0.0.0' >> ./build/.env;
+      echo 'NODE_ENV: development' >> ./build/.env;
+      echo 'APP_KEY: LVOkLnE8DTLl_Vn0tD6EEjbfkBSXpN7O' >> ./build/.env;
+      echo 'DRIVE_DISK: local' >> ./build/.env;
+      echo 'SESSION_DRIVER: cookie' >> ./build/.env;
+      echo 'CACHE_VIEWS: false' >> ./build/.env;
+      echo 'DB_CONNECTION: mysql' >> ./build/.env;
+      echo 'MYSQL_HOST: localhost' >> ./build/.env;
+      echo 'MYSQL_PORT:330' >> ./build/.env;
+      echo 'MYSQL_USER: root' >> ./build/.env;
+      echo 'MYSQL_PASSWORD: wecandobigtogether' >> ./build/.env;
+      echo 'MYSQL_DB_NAME: gosports_tim' >> ./build/.env;
+      pm2 reload ecosystem.config.js
+      `,
     },
   },
 };
