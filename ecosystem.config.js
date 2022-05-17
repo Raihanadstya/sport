b67cf0a575fb21017c0f1dd8fb14bdd732614a1e
Rochim <rochim.rci26@gmail.com>
2022-05-17 23:27:43 +0700

    fix bugs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       ref: "origin/main",
       key: "~/.ssh/id_rsa",
       "post-deploy":
-        "npm i; node ace build; node ace migration:run; echo 'PORT=4000' >> .env; echo 'HOST=0.0.0.0' >> .env; echo 'NODE_ENV=development' >> .env; echo 'APP_KEY=LVOkLnE8DTLl_Vn0tD6EEjbfkBSXpN7O' >> .env; echo 'DRIVE_DISK=local' >> .env; echo 'SESSION_DRIVER=cookie' >> .env; echo 'CACHE_VIEWS=false' >> .env; echo 'DB_CONNECTION=mysql' >> .env; echo 'MYSQL_HOST=localhost' >> .env;echo 'MYSQL_PORT=3306' >> .env; echo 'MYSQL_USER=root' >> .env; echo 'MYSQL_PASSWORD=wecandobigtogether' >> .env; echo 'MYSQL_DB_NAME=gosports_tim' >> .env; cp .env build/.env; pm2 reload ecosystem.config.js",
+        "npm i; node ace build; echo 'PORT=4000' >> .env; echo 'HOST=0.0.0.0' >> .env; echo 'NODE_ENV=development' >> .env; echo 'APP_KEY=LVOkLnE8DTLl_Vn0tD6EEjbfkBSXpN7O' >> .env; echo 'DRIVE_DISK=local' >> .env; echo 'SESSION_DRIVER=cookie' >> .env; echo 'CACHE_VIEWS=false' >> .env; echo 'DB_CONNECTION=mysql' >> .env; echo 'MYSQL_HOST=localhost' >> .env;echo 'MYSQL_PORT=3306' >> .env; echo 'MYSQL_USER=root' >> .env; echo 'MYSQL_PASSWORD=wecandobigtogether' >> .env; echo 'MYSQL_DB_NAME=gosports_tim' >> .env; cp .env build/.env; pm2 reload ecosystem.config.js",
     },
   },
 };
