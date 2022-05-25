0a48f1a9dfe95b772598bb4540afe7962316eb73
farrellh1 <farrell.hauzan@gmail.com>
2022-05-25 07:59:36 +0700

    fix pemain routes

--- start/routes/pemain.ts
+++ start/routes/pemain.ts
@@ -1,5 +1,3 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.resource("/pemain", "PemainsController").apiOnly();
-Route.post("/pemain/:timId", "PemainsController.store");
-// Route.put('/pemains/:timId/:id', 'PemainsController.update')
